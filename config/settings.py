0dfd5ab0b64f1850a67428b4867ecddb7f00f564
Lucas Pujada <lucebagames@gmail.com>
2025-12-03 13:33:00 -0300

    Update settings.py

--- config/settings.py
+++ config/settings.py
@@ -1,11 +1,1 @@
-from pydantic_settings import BaseSettings
 
-class Settings(BaseSettings):
-    REDIS_URL: str | None = None
-    CACHE_ENABLED: bool = True
-
-    class Config:
-        env_file = ".env"
-
-
-settings = Settings()
